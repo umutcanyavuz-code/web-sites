964fb1ad39cb289c1cad040a19e963a35dd9b352
umutcanyavuz-code <umutcan.yavuz@std.izmirekonomi.edu.tr>
2025-07-22 00:41:21 +0300

    Create homepage.php

---
+++ homepage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="anasayfa.php">Anasayfa</a>
+    <a href="sayfa.php">sayfa1</a>
+    <a href="kayıtsayfası.php">sayfa2</a>
+    <h1>anasayfadasın</h1>
+
+
+</body>
+</html>
+<?php
+include("baglanti.php");
+
+?>
